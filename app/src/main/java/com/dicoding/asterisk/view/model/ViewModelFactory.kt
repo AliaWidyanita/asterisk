080b21ab558110878148ba2b76a0a40458ab1544
nadhirashahnaz <nadhirashahnaz@ui.ac.id>
2024-06-13 16:41:59 +0700

    separating MainViewModel from ViewModelFactory

--- app/src/main/java/com/dicoding/asterisk/view/model/ViewModelFactory.kt
+++ app/src/main/java/com/dicoding/asterisk/view/model/ViewModelFactory.kt
@@ -4,26 +4,23 @@ import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.dicoding.asterisk.data.local.UserRepository
-import com.dicoding.asterisk.data.remote.ApiService
 import com.dicoding.asterisk.utils.Injection
-import com.google.android.gms.location.FusedLocationProviderClient
 
-class ViewModelFactory(private val repository: UserRepository, private val context: Context,private val apiService: ApiService,
-                       private val fusedLocationClient: FusedLocationProviderClient) : ViewModelProvider.NewInstanceFactory() {
+class ViewModelFactory(
+    private val repository: UserRepository,
+    private val context: Context
+) : ViewModelProvider.NewInstanceFactory() {
 
     @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         return when {
-            modelClass.isAssignableFrom(MainViewModel::class.java) -> {
-                MainViewModel(context, repository, apiService, fusedLocationClient) as T
-            }
-            modelClass.isAssignableFrom(RegisterViewModel::class.java)-> {
+            modelClass.isAssignableFrom(RegisterViewModel::class.java) -> {
                 RegisterViewModel(repository) as T
             }
             modelClass.isAssignableFrom(LoginViewModel::class.java) -> {
                 LoginViewModel(repository) as T
             }
-            modelClass.isAssignableFrom(AddReviewViewModel::class.java)-> {
+            modelClass.isAssignableFrom(AddReviewViewModel::class.java) -> {
                 AddReviewViewModel(repository) as T
             }
             else -> throw IllegalArgumentException("Unknown ViewModel class: " + modelClass.name)
@@ -33,11 +30,15 @@ class ViewModelFactory(private val repository: UserRepository, private val conte
     companion object {
         @Volatile
         private var INSTANCE: ViewModelFactory? = null
+
         @JvmStatic
-        fun getInstance(context: Context, apiService: ApiService, fusedLocationClient: FusedLocationProviderClient): ViewModelFactory {
+        fun getInstance(context: Context): ViewModelFactory {
             return INSTANCE ?: synchronized(this) {
-                INSTANCE ?: ViewModelFactory(Injection.provideRepository(context), context, apiService, fusedLocationClient)
-            }.also { INSTANCE = it }
+                INSTANCE ?: ViewModelFactory(
+                    Injection.provideRepository(context),
+                    context
+                ).also { INSTANCE = it }
+            }
         }
     }
 }
